dded3f144ab625d23424753f0bccfedd5b94f235
Uilian Ries <uilianries@gmail.com>
2019-03-22 11:35:37 -0300

    Bump version to 1.69

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
 
 
 class IMGUIConan(ConanFile):
     name = "imgui"
-    version = "1.62"
+    version = "1.69"
     url = "https://github.com/bincrafters/conan-imgui"
     homepage = "https://github.com/ocornut/imgui"
     description = "Bloat-free Immediate Mode Graphical User interface for C++ with minimal dependencies"
     author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "imgui", "gui", "graphical")
     license = "MIT"
     exports = ["LICENSE.md"]
-    exports_sources = ["CMakeLists.txt", "imgui_demo.h", "imgui_demo.cpp"]
+    exports_sources = ["CMakeLists.txt", ]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    source_subfolder = "source_subfolder"
+    default_options = {"shared": False, "fPIC": True}
+    _source_subfolder = "source_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -29,21 +28,23 @@ class IMGUIConan(ConanFile):
     def source(self):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.configure()
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        cmake = self.configure_cmake()
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="examples/imgui_impl_*", dst="misc/bindings", src=self._source_subfolder, keep_path=False)
+        cmake = self._configure_cmake()
         cmake.install()
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
 
     def package_info(self):
+        self.cpp_info.srcdirs = ["misc", ]
         self.cpp_info.libs = tools.collect_libs(self)
